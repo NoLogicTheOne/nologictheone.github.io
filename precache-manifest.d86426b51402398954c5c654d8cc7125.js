94d8a9b2fd2642e0494302ff181e98b25cf42611
NoLogicTheOne <monologic.loggy@yandex.ru>
2019-12-31 00:13:10 +0300

    add working app v1.0

---
+++ precache-manifest.d86426b51402398954c5c654d8cc7125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e766ddcb79d9f30aa0af17d234bd41c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6767a02fc3fcf1d4f0c6",
+    "url": "/static/css/main.fd500c3b.chunk.css"
+  },
+  {
+    "revision": "d6702a79f325334f0459",
+    "url": "/static/js/2.6216bf9e.chunk.js"
+  },
+  {
+    "revision": "6767a02fc3fcf1d4f0c6",
+    "url": "/static/js/main.82aebe53.chunk.js"
+  },
+  {
+    "revision": "f1d6387ad70a12020e6c",
+    "url": "/static/js/runtime-main.7d08b3fd.js"
+  }
+]);
